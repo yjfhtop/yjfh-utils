4b7e2dfc00234dbe3fd95a0d8aded71c94ecac24
yang <1762086020@qq.com>
2022-07-19 15:00:51 +0800

    update: 修改入口文件

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as dom from '@/utils/dom';
 import * as log from '@/utils/log';
+import * as date from '@/utils/date';
+import * as data from '@/utils/data';
+import * as optimization from '@/utils/optimization';
+import * as storage from '@/utils/storage';
 
 export default {
     dom,
     log,
+    date,
+    data,
+    optimization,
+    storage,
 };
